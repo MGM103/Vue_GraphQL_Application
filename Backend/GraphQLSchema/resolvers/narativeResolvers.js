349e39ccef1a49fa9561a2fa8dd18c02d60b3e94
Marcus Marinelli <marcus.marinelli3@gmail.com>
2023-04-23 22:17:28 +1000

    Adding error handling for Narative queries

--- Backend/GraphQLSchema/resolvers/narativeResolvers.js
+++ Backend/GraphQLSchema/resolvers/narativeResolvers.js
@@ -1,14 +1,34 @@
-import Narative from '../../Models/Narative.js';
-import Framework from '../../Models/Framework.js';
+import Narative from "../../Models/Narative.js";
+import Framework from "../../Models/Framework.js";
 
 const narativeResolvers = {
   Query: {
     async getNarativeByName(_, { name }) {
-        return await Narative.findOne({name: name});
+      try {
+        const result = await Narative.findOne({ name });
+
+        if (!result) {
+          throw new Error("Narative matching that name could not be found :(");
+        }
+
+        return result;
+      } catch (error) {
+        throw new Error(`Query getNarativeByName failed: ${error.message}`);
+      }
     },
     async getNarativeById(_, { _id }) {
-        return await Narative.findById(_id);
-    }
+      try {
+        const result = await Narative.findById(_id);
+
+        if (!result) {
+          throw new Error("Narative matching that Id could not be found :(");
+        }
+
+        return result;
+      } catch (error) {
+        throw new Error(`Query getNarativeById failed: ${error.message}`);
+      }
+    },
   },
   Mutation: {
     createNarative: (_, { narative }) => Narative.create(narative),
@@ -16,8 +36,8 @@ const narativeResolvers = {
       const deleteNarative = await Narative.deleteOne({ _id });
 
       await Framework.updateMany(
-        {naratives: {$in: [_id]}},
-        {$pull: {naratives: _id}}
+        { naratives: { $in: [_id] } },
+        { $pull: { naratives: _id } }
       );
 
       return deleteNarative.deletedCount === 1;
@@ -26,7 +46,7 @@ const narativeResolvers = {
       const narative = await Narative.findById(_id);
 
       Object.entries(newNarative).forEach(([key, value]) => {
-        if(value !== ""){
+        if (value !== "") {
           narative[key] = value;
         }
       });
@@ -34,8 +54,8 @@ const narativeResolvers = {
       await narative.save();
 
       return narative;
-    }
-  }
+    },
+  },
 };
 
 export default narativeResolvers;
--- Backend/tests/narativeQueries.test.js
+++ Backend/tests/narativeQueries.test.js
@@ -1,9 +1,9 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 import { initTestDb, shutDownTestDb } from "./test-utils/testDb";
-import { makeExecutableSchema } from '@graphql-tools/schema';
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import { graphql } from "graphql";
-import resolver from '../GraphQLSchema/resolvers/resolvers';
-import typeDef from '../GraphQLSchema/typeDefs/typeDefs';
+import resolver from "../GraphQLSchema/resolvers/resolvers";
+import typeDef from "../GraphQLSchema/typeDefs/typeDefs";
 
 describe("Test Narative Queries", () => {
   // global test mongodb instance
@@ -11,15 +11,16 @@ describe("Test Narative Queries", () => {
   let schema;
   const mockNarative = {
     name: "NFT-Fi",
-    description: "Unlocking the economic power of notable NFT collections by providing lending, CDPs, perps, etc."
+    description:
+      "Unlocking the economic power of notable NFT collections by providing lending, CDPs, perps, etc.",
   };
-  
+
   // Create & connect to test instance of mongodb
   // Then add mock data & create schema
   beforeAll(async () => {
     database = await initTestDb();
-    await database.collection('naratives').insertOne(mockNarative);
-    schema = makeExecutableSchema({typeDefs: typeDef, resolvers: resolver});
+    await database.collection("naratives").insertOne(mockNarative);
+    schema = makeExecutableSchema({ typeDefs: typeDef, resolvers: resolver });
   });
 
   // Destroy test instance of mongodb
@@ -29,12 +30,12 @@ describe("Test Narative Queries", () => {
 
   test("Get Narative by name", async () => {
     // create graphql query
-    const queryVars = {name: mockNarative.name};
+    const queryVars = { name: mockNarative.name };
     const query = gql`
-      query testGetNarativeByName($name: String!){
+      query testGetNarativeByName($name: String!) {
         getNarativeByName(name: $name) {
-          name,
-          description,
+          name
+          description
           _id
         }
       }
@@ -42,38 +43,95 @@ describe("Test Narative Queries", () => {
 
     // execute query against test schema
     const result = await graphql({
-      schema, 
+      schema,
       source: query.loc.source.body,
       contextValue: { database },
-      variableValues: queryVars
+      variableValues: queryVars,
     });
 
     expect(result.data.getNarativeByName.name).toEqual(mockNarative.name);
-    expect(result.data.getNarativeByName.description).toEqual(mockNarative.description);
-    expect(result.data.getNarativeByName._id).toEqual(mockNarative._id.toString());
+    expect(result.data.getNarativeByName.description).toEqual(
+      mockNarative.description
+    );
+    expect(result.data.getNarativeByName._id).toEqual(
+      mockNarative._id.toString()
+    );
+  });
+
+  test("Get Narative by Name error", async () => {
+    const queryVars = { name: "ForkDAO" };
+    const query = gql`
+      query testGetNarativeByName($name: String!) {
+        getNarativeByName(name: $name) {
+          name
+          description
+          _id
+        }
+      }
+    `;
+
+    // execute query against test schema
+    const result = await graphql({
+      schema,
+      source: query.loc.source.body,
+      contextValue: { database },
+      variableValues: queryVars,
+    });
+
+    expect(result.errors[0].message).toEqual(
+      "Query getNarativeByName failed: Narative matching that name could not be found :("
+    );
   });
 
   test("Get Narative by id", async () => {
-    const queryVars = {id: mockNarative._id.toString()};
+    const queryVars = { id: mockNarative._id.toString() };
     const query = gql`
       query testGetNarativeById($id: ID!) {
         getNarativeById(_id: $id) {
-          _id,
-          name,
+          _id
+          name
           description
         }
       }
     `;
 
     const result = await graphql({
-      schema, 
+      schema,
       source: query.loc.source.body,
       contextValue: { database },
-      variableValues: queryVars
+      variableValues: queryVars,
     });
-    
+
     expect(result.data.getNarativeById.name).toEqual(mockNarative.name);
-    expect(result.data.getNarativeById.description).toEqual(mockNarative.description);
-    expect(result.data.getNarativeById._id).toEqual(mockNarative._id.toString());
+    expect(result.data.getNarativeById.description).toEqual(
+      mockNarative.description
+    );
+    expect(result.data.getNarativeById._id).toEqual(
+      mockNarative._id.toString()
+    );
+  });
+
+  test("Get Narative by id", async () => {
+    const queryVars = { id: "6433e4b3447b87fd52a6c80c" };
+    const query = gql`
+      query testGetNarativeById($id: ID!) {
+        getNarativeById(_id: $id) {
+          _id
+          name
+          description
+        }
+      }
+    `;
+
+    const result = await graphql({
+      schema,
+      source: query.loc.source.body,
+      contextValue: { database },
+      variableValues: queryVars,
+    });
+
+    expect(result.errors[0].message).toEqual(
+      "Query getNarativeById failed: Narative matching that Id could not be found :("
+    );
   });
 });
