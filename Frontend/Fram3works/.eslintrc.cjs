25857745321eed6335544d9203d99eb38d0e4129
Marcus Marinelli <marcus.marinelli3@gmail.com>
2023-05-08 21:34:03 +1000

    Removing Vue bloat

--- Frontend/Fram3works/.eslintrc.cjs
+++ Frontend/Fram3works/.eslintrc.cjs
@@ -1,9 +1,9 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
@@ -14,12 +14,10 @@ module.exports = {
         '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
         'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
       ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      extends: ['plugin:cypress/recommended']
     }
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   }
-}
+};
